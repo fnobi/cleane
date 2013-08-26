287f25532ca1f4b0570183866353fd61c352ad0a
vagrant <vagrant@localhost.localdomain>
2013-08-26 09:00:41 +0000

    first commit.

---
+++ elispfile.el
@@ -0,0 +1,13 @@
+;; init repository
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+(list-packages)
+
+;; install packages
+(package-install 'js2-mode)
+(package-install 'ack)
+(package-install 'magit)
+(package-install 'markdown-mode)
+(package-install 'php-mode)
+(package-install 'zencoding-mode)
