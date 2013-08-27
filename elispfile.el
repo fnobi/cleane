40d18b866899301a97f2b6c490daf6213f371383
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-08-27 12:34:40 +0900

    最初にinitialize packageしておいた方がいいっぽい。

--- elispfile.el
+++ elispfile.el
@@ -1,3 +1,5 @@
+(package-initialize)
+
 ;; init repository
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
