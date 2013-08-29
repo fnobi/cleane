0e2109f8d371ec872fa5a2c2e56b30577bc5e909
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-08-30 01:37:19 +0900

    バックアップファイルを1箇所に集める設定

--- conf/init-defaults.el
+++ conf/init-defaults.el
@@ -1,2 +1,10 @@
 (setq-default indent-tabs-mode nil)
 (setq-default transient-mark-mode t)
+
+
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" , (expand-file-name "~/.emacs.d/backups/") t)))
+
