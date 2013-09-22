4766ee2d915550ad98786b87f87fa7cfb133d5eb
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-09-23 00:02:32 +0900

    goto-lineのキーバインド。

--- conf/init-keybinds.el
+++ conf/init-keybinds.el
@@ -7,6 +7,8 @@
 (define-key global-map (kbd "M-l") 'lisp-interaction-mode)
 (define-key global-map (kbd "M-i") 'indent-region)
 
+(define-key global-map (kbd "C-x :") 'goto-line)
+
 ;; library keybinds
 (define-key global-map (kbd "C-x g") 'magit-status)
 (define-key global-map (kbd "C-x j") 'eshell-clean-and-open)
