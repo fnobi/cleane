bd5c93283caf208390552d6aa86e335c48e77524
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-01-28 15:14:41 +0900

    auto-java-completeを追加。

--- init.el
+++ init.el
@@ -9,19 +9,24 @@
 (load "init-defaults")
 (load "init-shell")
 
+;; package initialize
+(package-initialize)
+
+;; load auto-java-complete
+(load "auto-java-complete/ajc-java-complete")
+(load "auto-java-complete/ajc-java-complete-config")
 
 ;; packages config
-(package-initialize)
 (load "init-css-mode")
 (load "init-js2-mode")
 (load "init-auto-complete")
 (load "init-emmet-mode")
 (load "init-anything")
+(load "init-auto-java-complete")
 
 ;; load libraries
 (load "eshell-clean-and-open")
 (load "my-make-scratch")
-
 
 ;; set keybinds
 (load "init-keybinds")
