e6ca494f23d7561279082de2d6888fd25715f224
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-02-18 15:04:17 +0900

    javaのauto-complete切った。

--- init.el
+++ init.el
@@ -12,17 +12,12 @@
 ;; package initialize
 (package-initialize)
 
-;; load auto-java-complete
-(load "auto-java-complete/ajc-java-complete")
-(load "auto-java-complete/ajc-java-complete-config")
-
 ;; packages config
 (load "init-css-mode")
 (load "init-js2-mode")
 (load "init-auto-complete")
 (load "init-emmet-mode")
 (load "init-anything")
-(load "init-auto-java-complete")
 
 ;; load libraries
 (load "eshell-clean-and-open")
