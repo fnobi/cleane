0385bf3c4fe202da26b23ca348072a843dbe480f
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-05-06 00:23:13 +0900

    open-urlしていきたい。

--- conf/init-keybinds.el
+++ conf/init-keybinds.el
@@ -7,13 +7,14 @@
 (define-key global-map (kbd "M-l") 'lisp-interaction-mode)
 (define-key global-map (kbd "M-i") 'indent-region)
 
+(define-key global-map (kbd "C-c C-l") 'toggle-truncate-lines)
 (define-key global-map (kbd "C-x :") 'goto-line)
+(define-key global-map (kbd "C-x f") 'find-file)
+(define-key global-map (kbd "M-o") 'browse-url)
 
 ;; library keybinds
 (define-key global-map (kbd "C-x g") 'magit-status)
 (define-key global-map (kbd "C-x j") 'eshell-clean-and-open)
 (define-key global-map (kbd "C-x C-f") 'anything-git-project)
 (define-key global-map (kbd "C-x ;") 'my-anything-find-file)
-(define-key global-map (kbd "C-c C-l") 'toggle-truncate-lines)
-(define-key global-map (kbd "C-x f") 'find-file)
 (define-key global-map (kbd "C-x p") 'clip-buffer)
