20e70d29adc955c256e2d0efb4e7dc2cd85000f8
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-06-15 16:17:16 +0900

    write-roughを頭良くする

--- lib/write-rough.el
+++ lib/write-rough.el
@@ -1,6 +1,14 @@
+(defun rough-file-name (index)
+  (setq dir (expand-file-name "~/Desktop"))
+  (setq today (shell-command-to-string "echo -n $(date +%y%m%d)"))
+  (setq filename (if (> index 0) 
+                   (format "%s/%s_rough-%d.md" dir today index)
+                   (format "%s/%s_rough.md" dir today)))
+  (if (file-exists-p filename) (rough-file-name (+ index 1)) filename))
+
 (defun write-rough ()
   "現在のbufferをいいかんじのファイル名で保存します"
   (interactive)
-  (setq rough (expand-file-name "~/Desktop/rough"))
+  (setq rough (rough-file-name 0))
   (write-file rough)
   (message (format "write to %s" rough)))
