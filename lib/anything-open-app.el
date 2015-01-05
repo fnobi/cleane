36e9e6c935eda0d6aec6095b787fd97e44c8aa16
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-01-05 14:24:51 +0900

    appはフルパスでopenする

--- lib/anything-open-app.el
+++ lib/anything-open-app.el
@@ -3,7 +3,7 @@
     (candidates . (lambda ()
                     (split-string (shell-command-to-string "mdfind kMDItemContentTypeTree==\"com.apple.application\"") "\n")))
     (type . file)
-    (action . (lambda (entry) (shell-command (format "open '%s'" entry))))))
+    (action . (lambda (entry) (shell-command (format "open '%s'" (expand-file-name entry)))))))
 
 (defun anything-open-app ()
   "Open application."
