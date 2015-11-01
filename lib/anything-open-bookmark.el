6440a7cf1329044460a89d792bd276c1c1083506
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-11-01 23:36:50 +0900

    top sitesをひらくanything

--- lib/anything-open-bookmark.el
+++ lib/anything-open-bookmark.el
@@ -1,15 +1,16 @@
-(defvar anything-c-source-bookmark-file
-  '((name . "Bookmarks")
+(defvar anything-c-source-topsites-file
+  '((name . "Top Sites")
     (candidates . (lambda ()
-                    (setq bookmark-file-path (expand-file-name "~/bookmark.txt"))
+                    (setq sqlite-path (expand-file-name "~/Library/Application\\ Support/Google/Chrome/Default/Top\\ Sites"))
+                    (setq sql "select url from thumbnails;")
                     (split-string (shell-command-to-string
-                                   (format "cat %s" bookmark-file-path)) "\n")))
+                                   (format "echo \"%s\" | sqlite3 %s" sql sqlite-path)) "\n")))
     (type . file)
     (action . (lambda (entry) (shell-command (format "open '%s'" entry))))))
 
-(defun anything-open-bookmark ()
-  "Open bookmark."
+(defun anything-open-topsites ()
+  "Open Top Sites."
   (interactive)
-  (anything (list anything-c-source-bookmark-file)
+  (anything (list anything-c-source-topsites-file)
             ""
-            "Open Bookmark: " nil))
+            "Open Topsites: " nil))
