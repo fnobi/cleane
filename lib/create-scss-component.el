41379d767c41deba169cff98bcf64d9a2cfa1c54
fujisawa-shin <fujisawa-shin@kayac.com>
2016-09-14 09:30:15 +0900

    create-scss-componentのディレクトリを変更

--- lib/create-scss-component.el
+++ lib/create-scss-component.el
@@ -3,15 +3,15 @@
   (interactive "sName: ")
 
   (setq pwd (shell-command-to-string "echo -n `pwd`"))
-  (setq components-dir-name "components")
+  (setq components-dir-name "module")
   (setq sass-components-dir (format "%s/%s" pwd components-dir-name))
   (setq sass-file-path (format "%s/_%s.scss" sass-components-dir name))
   (setq content (format ".%s {\n}" name))
 
   (cond
-   ((not (file-exists-p sass-components-dir)) (message "There is no components dir."))
+   ((not (file-exists-p sass-components-dir)) (message (format "There is no %s dir." components-dir-name)))
    (t ((lambda ()
         (write-region content nil sass-file-path)
-        (insert (format "@import \"components/%s\";" name))
+        (insert (format "@import \"%s/%s\";" components-dir-name name))
         (save-buffer)
         (find-file sass-file-path))))))
