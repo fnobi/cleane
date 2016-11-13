f44c53fb90e1b0cd4f6a051fc9a8daf2c728de49
fujisawa-shin <fujisawa-shin@kayac.com>
2016-11-13 20:46:46 +0900

    web-mode調整

--- conf/init-web-mode.el
+++ conf/init-web-mode.el
@@ -8,17 +8,10 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tag$"       . web-mode))
 
 ;; インデント関係
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   0)
-  (setq web-mode-css-offset    0)
-  (setq web-mode-script-offset 0)
-  (setq web-mode-php-offset    0)
-  (setq web-mode-java-offset   0)
-  (setq web-mode-asp-offset    0)
-  (setq indent-tabs-mode t)
-  (setq tab-width 0)
   (sgml-electric-tag-pair-mode t))
 (add-hook 'web-mode-hook 'web-mode-hook)
