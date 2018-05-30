e477165cd7e5d3fd26e9a70f09b531062daa98be
nobii <fujisawa-shin@kayac.com>
2018-05-30 18:53:31 +0900

    vueをweb-modeに加える

--- conf/init-web-mode.el
+++ conf/init-web-mode.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue$"       . web-mode))
 
 ;; インデント関係
 (defun web-mode-hook ()
@@ -18,3 +19,18 @@
   (setq web-mode-script-padding 2)
   (sgml-electric-tag-pair-mode t))
 (add-hook 'web-mode-hook 'web-mode-hook)
+
+(setq web-mode-ac-sources-alist
+      '(("php" . (ac-source-yasnippet ac-source-php-auto-yasnippets))
+        ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+        ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+                   (yas-activate-extra-mode 'php-mode)
+                 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+                 (setq emmet-use-css-transform nil)))))
